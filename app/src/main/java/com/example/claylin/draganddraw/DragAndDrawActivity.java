6121baba2e4bc09296f22f0ae4fc847d2fd42f8c
claylin <claylin.007@gmail.com>
2015-04-19 15:07:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/claylin/draganddraw/DragAndDrawActivity.java
@@ -0,0 +1,16 @@
+package com.example.claylin.draganddraw;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class DragAndDrawActivity extends SingleFragmentActivity {
+
+    @Override
+    public Fragment createFragment() {
+        return new DragAndDrawFragment();
+    }
+}
