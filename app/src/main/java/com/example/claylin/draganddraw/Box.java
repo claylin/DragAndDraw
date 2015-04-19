8c456f255f6f1a63caa0cb78ad14db526c8d1e46
claylin <claylin.007@gmail.com>
2015-04-19 15:44:39 +0800

    Draw boxes on the view, page 547.

---
+++ app/src/main/java/com/example/claylin/draganddraw/Box.java
@@ -0,0 +1,27 @@
+package com.example.claylin.draganddraw;
+
+import android.graphics.PointF;
+
+/**
+ * Created by claylin on 4/19/15.
+ */
+public class Box {
+    private PointF mOrigin;
+    private PointF mCurrent;
+
+    public Box(PointF origin) {
+        mOrigin = mCurrent = origin;
+    }
+
+    public PointF getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(PointF current) {
+        mCurrent = current;
+    }
+
+    public PointF getOrigin() {
+        return mOrigin;
+    }
+}
